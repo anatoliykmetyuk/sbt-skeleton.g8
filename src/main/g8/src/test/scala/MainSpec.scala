5e58fc0d01e23ef577007667c8f3556a41ea729a
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2016-10-20 23:15:55 +0300

    Adaptation to my style

---
+++ src/main/g8/src/test/scala/MainSpec.scala
@@ -0,0 +1,11 @@
+package $organization$.$name;format="lower,word"$
+
+import org.scalatest._
+
+class MainSpec extends FlatSpec with Matchers {
+
+  "True" should "be true" in {
+    true shouldBe true
+  }
+
+}
